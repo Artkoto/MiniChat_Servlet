d602999ae139dd49e7c858722f406f72ae4c451c
yakoto <yao-arnaud.akoto@etudiant.univ-rennes1.fr>
2021-04-09 01:18:58 +0200

    reste uniquement à mettre du style et faire le rapport

--- src/main/java/pr/tp/web/servlet/Chat.java
+++ src/main/java/pr/tp/web/servlet/Chat.java
@@ -7,18 +7,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
 import java.util.Date;
 
 @WebServlet("/chat")
 public class Chat extends HttpServlet {
 	private static final long serialVersionUID = 197811968639586823L;
 	private StringBuffer chatContent;
+	private static  int  numberOfConnection = 0;
+	private static String path = "save.txt";
 
 	@Override
 	public void init() throws ServletException {
 		chatContent = new StringBuffer();
-		chatContent.append(getServletConfig().getServletContext().getInitParameter("welcome")).append("\n\n");
+		try {
+			FileReader file = new FileReader(path);
+			BufferedReader bufferedReader = new BufferedReader(file);
+			String ligne;
+			while ((ligne = bufferedReader.readLine()) != null){
+				chatContent.append(ligne).append("\n");
+			}
+			if (chatContent.toString().equals("")) {
+				chatContent.append(getServletConfig().getServletContext().getInitParameter("welcome")).append("\n\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			chatContent.append(getServletConfig().getServletContext().getInitParameter("welcome")).append("\n\n");
+		}
+
 	}
 
 	@Override
@@ -62,22 +78,40 @@ public class Chat extends HttpServlet {
 					session = req.getSession(true);
 					chatContent.append(getDate().toString()).append("\t").append(user).append(" a rejoint le chat").append("\n");
 					session.setAttribute("user", user);
+					numberOfConnection++;
 				}
 				break;
 			}
 			//deconnexion
 			case "logout": {
 				session = req.getSession(false);
+				numberOfConnection--;
+				if (numberOfConnection == 0){
+					this.destroy();
+				}
 				String user = session.getAttribute("user").toString();
 				chatContent.append(getDate().toString()).append("\t").append(user).append(" a quitté le chat").append("\n");
 				session.setAttribute("user", null);
+
 				break;
 			}
 		}
 		this.doGet(req, resp);
 	}
 
+	@Override
+	public void destroy() {
+//		super.destroy();
+		try {
+			PrintWriter writer = new PrintWriter(path);
+			writer.print(chatContent.toString());
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
+
+	}
 
 	public Date getDate(){
 		return new Date();
